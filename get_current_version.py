de72f6a22bdee63dda6bcc03108a3f2de6706df6
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-10-11 21:46:06 +0500

    Add GLFW and GLM packages #4

--- get_current_version.py
+++ get_current_version.py
@@ -3,6 +3,8 @@ import os
 package_to_version = {
       'entt': 'entt/3.8.1@snv/stable',
       'glad': 'glad/0.1.34@snv/stable',
+      'glfw': 'glfw/3.3.4@snv/stable',
+       'glm': 'glm/0.9.9.8@snv/stable',
      'imgui': 'imgui/18410@snv/docking',
     'spdlog': 'spdlog/1.9.2@snv/stable',
        'stb': 'stb/cci.20210713@snv/stable',
