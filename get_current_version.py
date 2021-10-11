3d6099c4e936b532800b9990e5e736dc23b475e7
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-10-11 21:30:12 +0500

    1

--- get_current_version.py
+++ get_current_version.py
@@ -1,9 +1,11 @@
 import os
 
 package_to_version = {
+      'entt': 'entt/3.8.1@snv/stable',
       'glad': 'glad/0.1.34@snv/stable',
      'imgui': 'imgui/18410@snv/docking',
-    'spdlog': 'spdlog/1.9.2@snv/stable'
+    'spdlog': 'spdlog/1.9.2@snv/stable',
+       'stb': 'stb/cci.20210713@snv/stable',
 }
 
 package_name = os.environ['PACKAGE_NAME']
