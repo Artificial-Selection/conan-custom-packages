2a279111f92708e670f8db9045041556f2485836
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-10-11 18:20:48 +0500

    1

--- generate_matrix.py
+++ generate_matrix.py
@@ -2,23 +2,38 @@ import os
 import subprocess
 
 
+GITHUB_WORKFLOWS_PATH = "./.github/workflows/"
 PACKAGES_PATH = "./packages/"
+
+PREVIOUS_SHA = os.environ['PREVIOUS_SHA']
+CURRENT_SHA  = os.environ['CURRENT_SHA']
 
 
 def check_changes(package_path):
-    git_diff_output = subprocess.check_output(['git', 'diff', "--name-only", package_path])
+    with open(os.devnull, 'wb') as shutup:
+        git_diff_output = subprocess.check_output(['git', 'diff', '--name-only', PREVIOUS_SHA, CURRENT_SHA, '--', package_path], stderr=shutup)
     if git_diff_output:
         return True
     return False
 
+
+all_packages = []
 packages_to_build = []
 
 for package_name in os.listdir(PACKAGES_PATH):
     package_path = os.path.join(PACKAGES_PATH, package_name)
 
-    if os.path.isdir(package_path) and check_changes(package_path):
-        packages_to_build.append(package_name)
+    if os.path.isdir(package_path):
+        all_packages.append(package_name)
+        if check_changes(package_path):
+            packages_to_build.append(package_name)
 
-matrix_json = '{"package": ["' + '", "'.join(packages_to_build) + '"]}'
+# if github actions build script changed, rebuild all packages
+if check_changes(GITHUB_WORKFLOWS_PATH):
+    selected_list = all_packages
+else:
+    selected_list = packages_to_build
+
+matrix_json = '{"package": ["' + '", "'.join(selected_list) + '"]}'
 
 print(matrix_json)
---
+++ packages/imgui/conanfile.py
@@ -0,0 +1,101 @@
+from conans import ConanFile, CMake, tools
+import os
+import shutil
+
+required_conan_version = ">=1.33.0"
+
+
+class IMGUIConan(ConanFile):
+    name = "imgui"
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/ocornut/imgui"
+    description = "Bloat-free Immediate Mode Graphical User interface for C++ with minimal dependencies"
+    topics = ("conan", "imgui", "gui", "graphical")
+    license = "MIT"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+         "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+
+#-- Add glfw for backends
+    requires = [
+        'glfw/3.3.4',
+    ]
+#--
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  destination=self._source_subfolder, strip_root=True)
+
+#-- Copy backend files
+        backends_folder = os.path.join(self._source_subfolder, "backends")
+        backends_files = [
+            "imgui_impl_glfw",
+            "imgui_impl_opengl3",
+            "imgui_impl_dx11",
+            "imgui_impl_dx12",
+        ]
+
+        for file_name in backends_files:
+            shutil.move(os.path.join(backends_folder, f'{file_name}.h'), self._source_subfolder)
+            shutil.move(os.path.join(backends_folder, f'{file_name}.cpp'), self._source_subfolder)
+
+        shutil.move(os.path.join(backends_folder, "imgui_impl_opengl3_loader.h"), self._source_subfolder)
+#--
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        # backends_folder = os.path.join(
+        #     self._source_subfolder,
+        #     "backends" if tools.Version(self.version) >= "1.80" else "examples"
+        # )
+        # self.copy(pattern="imgui_impl_*",
+        #           dst=os.path.join("res", "bindings"),
+        #           src=backends_folder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["imgui"]
+        self.cpp_info.defines.append("IMGUI_USER_CONFIG=\"imgui_user_config.h\"")
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs.append("m")
+        # self.cpp_info.srcdirs = [os.path.join("res", "bindings")]
+
+        bin_path = os.path.join(self.package_folder, "bin")
+        self.output.info("Appending PATH env var with : {}".format(bin_path))
+        self.env_info.PATH.append(bin_path)
