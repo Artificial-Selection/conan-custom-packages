35808c574adc6a86358030badc8022adc6a12392
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2021-10-11 21:12:52 +0500

    1

--- get_current_version.py
+++ get_current_version.py
@@ -1,8 +1,9 @@
 import os
 
 package_to_version = {
-     'glad': 'glad/0.1.34@snv/stable',
-    'imgui': 'imgui/18410@snv/docking'
+      'glad': 'glad/0.1.34@snv/stable',
+     'imgui': 'imgui/18410@snv/docking',
+    'spdlog': 'spdlog/1.9.2@snv/stable'
 }
 
 package_name = os.environ['PACKAGE_NAME']
